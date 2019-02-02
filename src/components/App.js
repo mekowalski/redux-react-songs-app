60934c80a293256d3cd937fb78de7ff62faba0b7
Malind Kowalski <malind.nikulski@gmail.com>
2019-02-02 15:11:46 -0700

    minor change

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+//functional component because this App will NOT deal with State sine Redux will be integrated
 const App = () => {
   return <div>This is the Songs App</div>
 };
