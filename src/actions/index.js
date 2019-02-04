31b68b47ebd65ee6b78e34086c425052c2b3f258
Malind Kowalski <malind.nikulski@gmail.com>
2019-02-04 15:51:15 -0700

    create actions/index.js

---
+++ src/actions/index.js
@@ -0,0 +1,5 @@
+//this is an unfortunate pattern in React projectss
+//eventually write some code in this files
+//then import it somewhere else inside project
+// eg: import actions from '../actions' because there is only 1 file in this DIR
+//If a file isn't specified Webpack will automatically give the index.js file
