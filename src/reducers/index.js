418bb717574938e65073668493c8e5081c34a9b2
Malind Kowalski <malind.nikulski@gmail.com>
2019-02-04 16:13:53 -0700

    add static array

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,6 +7,8 @@ const songsReducer = () => {
   //static array, no actions, no reason to change
   //an array of objects, each object represents a song
   return [
-    {}
+    {title: 'Winning Streak', duration: '3:19'},
+    {title: 'Blowback', duration: '4:05'},
+    {title: 'Belong To You', duration: '3:07'}
   ]
 }
