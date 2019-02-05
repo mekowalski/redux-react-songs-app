db2245c4b37b62b348147b997a29fa70354461a7
Malind Kowalski <malind.nikulski@gmail.com>
2019-02-05 14:02:43 -0700

    begin configuration

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -11,6 +11,13 @@ class SongList extends React.Component {
   }
 }
 
+//take State object, all the Data in Store
+//run a calculation on it to show it up as Props inside Component
+//can be named any thing ie: getMyState but convention is this name
+const mapStateToProps = () => {
+
+}
+
 export default connect()(SongList)
 //connect syntax
 //connect(): returns a function
