b48ef23fc1bc1048f89c6018d3110df01eddde75
Malind Kowalski <malind.nikulski@gmail.com>
2019-02-06 14:57:37 -0700

    minor notes on SongDetail

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -18,8 +18,11 @@ const SongDetail = ({ song }) => {
   );
 }
 
+//wrap SongDetail with connet() to get State/Data from Store
+//the only property this component cares about is the selectedSong
 const mapStateToProps = state => {
   return { song: state.selectedSong }
 }
 
+//mapStateToProps: SongDetail will get a props object that contains the currently selected song
 export default connect(mapStateToProps)(SongDetail)
