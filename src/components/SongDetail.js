2abe92016b05483148903a6f9a24bbedfe3dee2a
Malind Kowalski <malind.nikulski@gmail.com>
2019-02-06 15:05:39 -0700

    remove comments

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,7 +1,6 @@
 //React Side
 import React from 'react';
 import { connect } from 'react-redux';
-//connect can work with both class-based and functional components
 
 const SongDetail = ({ song }) => {
   if (!song) {
@@ -18,11 +17,8 @@ const SongDetail = ({ song }) => {
   );
 }
 
-//wrap SongDetail with connet() to get State/Data from Store
-//the only property this component cares about is the selectedSong
 const mapStateToProps = state => {
   return { song: state.selectedSong }
 }
 
-//mapStateToProps: SongDetail will get a props object that contains the currently selected song
 export default connect(mapStateToProps)(SongDetail)
